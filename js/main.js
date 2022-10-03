003b459aa78d2e1db63a8a813684f615ecb50418
Vilenan <vilenan@mail.ru>
2022-10-03 12:20:21 +0300

    Написала вспомогательные функции по возвращению целого числа из диапазона и проверке длины строки

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//Функция, возвращающая случайное целое число из переданного диапазона включительно
+
+const getRandomNumber = function (a, b){
+  if((a < 0) || (b < 0) || (a === b)){
+    return NaN;
+  }
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+getRandomNumber(34.7, 8.9);
+//Функция, проверяющая максимальную длину строки. Результат: true, если строка проходит по длине, и false — если не проходит
+const checkStringLength = function (string, maxlength){
+  return string.length <= maxlength;
+};
+checkStringLength('hello', 9);
