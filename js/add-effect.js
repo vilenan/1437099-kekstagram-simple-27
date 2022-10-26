66973394cb90c52cdbf9f16201ad6c794791ae30
Vilenan <vilenan@mail.ru>
2022-10-26 15:39:55 +0300

    добавила возможность регулировать глубину эффекта на фото

--- js/add-effect.js
+++ js/add-effect.js
@@ -1,69 +1,120 @@
 const form = document.querySelector('.img-upload__form');
 const previewEl = form.querySelector('.img-upload__preview img');
 
-const effectNone = form.querySelector('#effect-none');
-const effectChrome = form.querySelector('#effect-chrome');
-const effectSepia = form.querySelector('#effect-sepia');
-const effectMarvin = form.querySelector('#effect-marvin');
-const effectPhobos = form.querySelector('#effect-phobos');
-const effectHeat = form.querySelector('#effect-heat');
-
 const zoomOutBtn = form.querySelector('.scale__control--smaller');
 const zoomInBtn = form.querySelector('.scale__control--bigger');
 const scaleValueEl = form.querySelector('.scale__control--value');
-// scaleValueEl.value = '100';
-// console.log(`scale(${(scaleValueEl.value) / 100})`);
-
-// previewEl.style.transform = `scale(${(scaleValueEl.value) / 100})`;
 
 const sliderEl = form.querySelector('.effect-level__slider');
+const effectValueEl = form.querySelector('.effect-level__value');
+
 
 const SCALE_STEP = 25;
-
-// noUiSlider.create(sliderEl, {
-//   range: {
-//     min: 25,
-//     max: 100
-//   },
-//   start: 100,
-//   step: 25,
-//   connect: 'lower',
-// });
-
-const addEffect = function (evt){
-  if(evt.target === effectNone){
-    previewEl.className = '';
-    previewEl.classList.add('effects__preview--none');
-  }
-  if(evt.target === effectChrome){
-    previewEl.className = '';
-    previewEl.classList.add('effects__preview--chrome');
-  } else
-  if(evt.target === effectSepia){
-    previewEl.className = '';
-    previewEl.classList.add('effects__preview--sepia');
-  } else
-  if(evt.target === effectMarvin){
-    previewEl.className = '';
-    previewEl.classList.add('effects__preview--marvin');
-  }
-  if(evt.target === effectPhobos) {
-    previewEl.className = '';
-    previewEl.classList.add('effects__preview--phobos');
-  }
-  if(evt.target === effectHeat) {
-    previewEl.className = '';
-    previewEl.classList.add('effects__preview--heat');
-  }
+const effects = {
+  none : {
+    name: 'none',
+    range: {
+      min: 0,
+      max: 1
+    },
+    start: 1,
+    step: 0.1,
+    filter: '',
+    prefix: '',
+  },
+  chrome : {
+    name: 'chrome',
+    range: {
+      min: 0,
+      max: 1
+    },
+    start: 1,
+    step: 0.1,
+    filter: 'grayscale',
+    prefix: '',
+  },
+  sepia: {
+    name: 'sepia',
+    range: {
+      min: 0,
+      max: 1
+    },
+    start: 1,
+    step: 0.1,
+    filter: 'sepia',
+    prefix: '',
+  },
+  marvin: {
+    name: 'marvin',
+    range: {
+      min: 0,
+      max: 100
+    },
+    start: 100,
+    step: 1,
+    filter: 'invert',
+    prefix: '%',
+  },
+  phobos: {
+    name: 'phobos',
+    range: {
+      min: 0,
+      max: 3,
+    },
+    start: 3,
+    step: 0.1,
+    filter: 'blur',
+    prefix: 'px',
+  },
+  heat: {
+    name: 'heat',
+    range: {
+      min: 1,
+      max: 3,
+    },
+    start: 3,
+    step: 0.1,
+    filter: 'brightness',
+    prefix: '',
+  },
 };
 
-// sliderEl.noUiSlider.on('update', () => {
-//   scaleValueEl.value = sliderEl.noUiSlider.get(true);
-//   previewEl.style.transform = `scale(${(scaleValueEl.value) / 100})`;
-// });
+noUiSlider.create(sliderEl, {
+  range: {
+    min: 0,
+    max: 1
+  },
+  start: 1,
+  connect: 'lower',
+});
+let currentEffect;
 
+const addEffect = function (evt){
+  currentEffect = effects[evt.target.value]; //какой эффект выбран heat, объект
+  previewEl.className = '';
+  previewEl.classList.add(`effects__preview--${evt.target.value}`);
+  sliderEl.noUiSlider.updateOptions({
+    range: {
+      min: currentEffect.range.min,
+      max: currentEffect.range.max,
+    },
+    start: currentEffect.start,
+    step: currentEffect.step,
+  });
+  return currentEffect;
+};
 
-// const scaleValueNum = parseInt(scaleValueEl.value,10);
+sliderEl.noUiSlider.on('update', () => {
+  const effectValue = sliderEl.noUiSlider.get(true);
+  effectValueEl.value = effectValue;
+  if(currentEffect && currentEffect.name !== effects.none.name){
+    sliderEl.removeAttribute('disabled');
+    previewEl.style.filter = `${currentEffect.filter}(${effectValue}${currentEffect.prefix})`;
+  } else {
+    previewEl.style.filter = '';
+    sliderEl.setAttribute('disabled', true);
+  }
+});
 
 const increaseImage = function (){
   let scale = parseInt(scaleValueEl.value,10);
