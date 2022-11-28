ce5daf80b804d2e3e19b9bd8bd302e564f4fb9a0
Vilenan <vilenan@mail.ru>
2022-11-28 19:20:47 +0300

    добавила сценарий просмотра фотографий в полноразмерном режиме

--- js/render-posts.js
+++ js/render-posts.js
@@ -1,12 +1,51 @@
 const postsContainer = document.querySelector('.pictures');
 const postTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const renderPost = (item) => {
+
+//отрисовка полноразмерного поста
+const bigPicture = document.querySelector('.big-picture');
+const bigPictureCloseBtn = bigPicture.querySelector('.big-picture__cancel');
+const bigPictureImage = bigPicture.querySelector('.big-picture__img img');
+const likesCount = bigPicture.querySelector('.likes-count');
+const commentsCount = bigPicture.querySelector('.comments-count');
+// const commentsList = bigPicture.querySelector('.social__comments');
+const bigPictureDescription = bigPicture.querySelector('.social__caption');
+// const commentTemplate = `<li class="social__comment">
+//     <img
+//         class="social__picture"
+//         src="{{аватар}}"
+//         alt="{{имя комментатора}}"
+//         width="35" height="35">
+//     <p class="social__text">{{текст комментария}}</p>
+// </li>`;
+
+
+function closeBigPictureModal(){
+  bigPicture.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  bigPictureCloseBtn.removeEventListener('click', closeBigPictureModal);
+}
+
+function openBigPictureModal(){
+  bigPicture.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  bigPictureCloseBtn.addEventListener('click', closeBigPictureModal);
+}
+
+const onClickPost = ({ url, description, likes, comments }) => {
+  openBigPictureModal();
+  bigPictureImage.src = url;
+  bigPictureDescription.textContent = description;
+  likesCount.textContent = likes;
+  commentsCount.textContent = comments;
+};
+
+const renderPost = ({ url, description, likes, comments }) => {
   const post = postTemplate.cloneNode(true);
   const postImage = post.querySelector('.picture__img');
-  postImage.src = item.url;
-  postImage.alt = item.description;
-  post.querySelector('.picture__comments').textContent = `${item.comments}`;
-  post.querySelector('.picture__likes').textContent = `${item.likes}`;
+  postImage.src = url;
+  postImage.alt = description;
+  post.querySelector('.picture__comments').textContent = comments;
+  post.querySelector('.picture__likes').textContent = likes;
   return post;
 };
 
@@ -14,6 +53,7 @@ const renderPosts = (posts) => {
   const fragment = document.createDocumentFragment();
   posts.forEach((post) => {
     const newPost = renderPost(post);
+    newPost.addEventListener('click', () => onClickPost(post));
     fragment.append(newPost);
   });
   postsContainer.append(fragment);
