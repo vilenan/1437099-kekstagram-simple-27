9d47f7df99e5273ede2ce24e2c94ba9e601d2a4d
Vilenan <vilenan@mail.ru>
2022-11-28 14:46:14 +0300

    версия с expressЭ lsdj

--- server.js/index.js
+++ server.js/index.js
@@ -1,31 +1,31 @@
 
-const http = require('http');
-// const express = require('express');
-// const app = express;
-const server = http.createServer(function (request, response){
-  //выводим в консоль путь, на который отправили запрос
-  const data = [
-    {
-      name: 'Ivan',
-      likes: 22,
-    },
-    {
-      name: 'Alex',
-      likes: 25,
-    }
-  ];
-  //как мы хотим ответить response.end
-
-  if(request.url === '/kekstagram-simple' && request.method === 'POST'){
-    response.end('Kekstagram Simple');
-    const body = request.read();
-    console.log(body);
-    response.end(body);
-  } else if( request.url === '/kekstagram-simple/data' ){
-    response.end(JSON.stringify(data));
-  }
-});
-server.listen(8000);
+// const http = require('http');
+// // const express = require('express');
+// // const app = express;
+// const server = http.createServer(function (request, response){
+//   //выводим в консоль путь, на который отправили запрос
+//   const data = [
+//     {
+//       name: 'Ivan',
+//       likes: 22,
+//     },
+//     {
+//       name: 'Alex',
+//       likes: 25,
+//     }
+//   ];
+//   //как мы хотим ответить response.end
+//
+//   if(request.url === '/kekstagram-simple' && request.method === 'POST'){
+//     response.end('Kekstagram Simple');
+//     const body = request.read();
+//     console.log(body);
+//     response.end(body);
+//   } else if( request.url === '/kekstagram-simple/data' ){
+//     response.end(JSON.stringify(data));
+//   }
+// });
+// server.listen(8000);
 
 // let fs = require('fs');
 // http.createServer(function(request, response) {
@@ -51,3 +51,29 @@ server.listen(8000);
 //     }
 //   });
 // }).listen(80);
+
+const express = require('express');
+const cors = require('cors');
+const myServer = express;
+
+myServer.use(cors());
+
+myServer.post('/kekstagram-simple',(request, response) => {
+  response.end('kekstagram-simple');
+});
+
+myServer.get('/kekstagram-simple/data',(request, response) => {
+  const data = [
+    {
+      name: 'Ivan',
+      likes: 22,
+    },
+    {
+      name: 'Alex',
+      likes: 25,
+    }
+  ];
+  response.end(JSON.stringify(data));
+});
+
+myServer.listen(8000);
