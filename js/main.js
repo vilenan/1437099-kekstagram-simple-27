a4c0fa470e2e1090ec9ca97c45cdf74e9593a233
Vilenan <vilenan@mail.ru>
2022-12-05 17:57:45 +0300

    добавила интерфейс фильтров в отдельный модуль

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-import {init} from './render-posts.js';
+import {setFilterClick} from './filters.js';
+import {renderPosts} from './render-posts.js';
 import {showAlert} from './utils.js';
 import './form.js';
 import {getData} from './api.js';
+
+const init = (posts) =>{
+  renderPosts(posts);
+  setFilterClick(posts);
+};
 
 getData(init, showAlert);
 
--- js/render-posts.js
+++ js/render-posts.js
@@ -1,26 +1,8 @@
 import {onPostClick} from './render-big-photo-popup.js';
-import {shuffle} from './utils.js';
 
-const RANDOM_POSTS_TO_SHOW = 10;
+const filters = document.querySelector('.img-filters');
 const postsContainer = document.querySelector('.pictures');
 const postTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const filtersContainer = document.querySelector('.img-filters__form');
-const filters = document.querySelector('.img-filters');
-const randomFilter = filters.querySelector('#filter-random');
-const initialFilter = filters.querySelector('#filter-default');
-const discussedFilter = filters.querySelector('#filter-discussed');
-
-const setFilterClick = () => {
-  filtersContainer.addEventListener('click', (evt) => {
-    const activeFilter = document.querySelector('.img-filters__button--active');
-    if(activeFilter){
-      activeFilter.classList.remove('img-filters__button--active');
-    }
-    const filter = evt.target.closest('.img-filters__button');
-    filter.classList.add('img-filters__button--active');
-  });
-};
-
 const renderPost = ({ url, description, likes, comments }) => {
   const post = postTemplate.cloneNode(true);
   const postImage = post.querySelector('.picture__img');
@@ -31,43 +13,7 @@ const renderPost = ({ url, description, likes, comments }) => {
   return post;
 };
 
-const makeRandomArr = (array) => {
-  let randomArr = [];
-  const randomCount = Math.min(array.length, RANDOM_POSTS_TO_SHOW);
-  const arrayOfIndex = Array.from({ length: array.length }, (v, k) => k);
-  randomArr = shuffle(arrayOfIndex).slice(0, randomCount);
-  return randomArr.map((index) => array[index]);
-};
-
-const makeInitialArr = (array) => array;
-
-const makeDiscussedArray = (array) => array.slice().sort((prevEl, nextEl) => nextEl.comments.length - prevEl.comments.length);
-
-const setRandomFilterClick = (cb) => {
-  randomFilter.addEventListener('click',() => {
-    postsContainer.textContent = '';
-    const randomArr = cb();
-    renderPosts(randomArr);
-  });
-};
-
-const setDefaultFilterClick = (cb) => {
-  initialFilter.addEventListener('click',() => {
-    postsContainer.textContent = '';
-    const defaultArr = cb();
-    renderPosts(defaultArr);
-  });
-};
-
-const setDiscussedFilterClick = (cb) => {
-  discussedFilter.addEventListener('click',() => {
-    postsContainer.textContent = '';
-    const discussedArr = cb();
-    renderPosts(discussedArr);
-  });
-};
-
-function renderPosts(posts){
+const renderPosts = (posts) => {
   const fragment = document.createDocumentFragment();
   posts.forEach((post) => {
     const newPost = renderPost(post);
@@ -76,14 +22,6 @@ function renderPosts(posts){
   });
   postsContainer.append(fragment);
   filters.classList.remove('img-filters--inactive');
-}
-
-const init = (posts) =>{
-  renderPosts(posts);
-  setFilterClick();
-  setRandomFilterClick(() => makeRandomArr(posts));
-  setDefaultFilterClick(() => makeInitialArr(posts));
-  setDiscussedFilterClick(() => makeDiscussedArray(posts));
 };
 
-export {init};
+export {renderPosts, postsContainer};
